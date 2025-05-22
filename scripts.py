bd9d737e55c2ab6c2ce4974a003bef1d2f05611d
pereskokovae <pereskokovae48@gmail.com>
2025-05-22 20:48:03 +0700

    removed unnecessary files, moved praise into a separate constant and added a check for the existence of an object

--- scripts.py
+++ scripts.py
@@ -1,11 +1,19 @@
 import random
 
-from datacenter.models import (Chastisement, Commendation, Lesson, Mark,
-                               Schoolkid)
+from django.core.exceptions import ObjectDoesNotExist
+from datacenter.models import (Chastisement, Commendation, Lesson, Mark,                            
+                                Schoolkid)
+
+
+RANDOM_PRAISE = random.choice(["Хвалю!", "Молодец!", "Хорошая работа!", "Прекрасно!", "Так держать!"])
 
 
 def fix_marks(schoolkid):
-    schoolkid = Schoolkid.objects.get(full_name__contains="Фролов Иван")
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains="Фролов Иван")
+    except ObjectDoesNotExist:
+        print("Ученик не найден")
+        
     bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__lte=3)
     bad_marks.update(points=5)
     return schoolkid
@@ -18,15 +26,17 @@ def remove_chastisements(schoolkid):
 
 
 def create_commendation(schoolkid):
-    lessons = list(Lesson.objects.filter(year_of_study=6))
-    praises = ["Хвалю!", "Молодец!", "Хорошая работа!", "Прекрасно!", "Так держать!"]
-
-    praise = random.choice(praises)
+    lessons = list(Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter
+        ))
+    if not lessons:
+        print("Уроки не найдены")
     subject = random.choice(lessons)
     Commendation.objects.create(
-        text=praise,
-        created=subject.date,
-        schoolkid=schoolkid,
-        subject=subject.subject,
-        teacher=subject.teacher
-        ) 
+            text=RANDOM_PRAISE,
+            created=subject.date,
+            schoolkid=schoolkid,
+            subject=subject.subject,
+            teacher=subject.teacher
+            )
